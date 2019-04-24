08ec6ca2dd52381ffbffae05f25f3234f1fb5a19
Ka Yu Li <kayuli@umass.edu>
2019-04-24 04:03:25 -0400

    added audios and videos

--- play.js
+++ play.js
@@ -1,8 +1,20 @@
+var num = Math.floor(Math.random()*10);
+if (num <= 4){
+  document.querySelector("#video").src = "/videos/captain_america.mp4"
+  document.querySelector("#audio").src = "/audios/captain_america.mp3"
+}
+
+else if (num > 4){
+  document.querySelector("#video").src = "/videos/ironman.mp4"
+  document.querySelector("#audio").src = "/audios/ironman.mp3"
+}
+
 
 
 setTimeout(function(){
-    document.getElementById('video').play();
+    var vid = document.getElementById('video')
+    vid.muted = true;
+    vid.play();
     var audio = document.getElementById("audio");
     audio.play();
-    console.log('test')
-  }, 1000);
+  }, 250);
