9ed70431e1ba82f19743f2cf210e190c9f69aeed
Ka Yu Li <kayuli@umass.edu>
2019-04-24 15:51:05 -0400

    thor

--- play.js
+++ play.js
@@ -1,12 +1,22 @@
 var num = Math.floor(Math.random()*10);
-if (num <= 4){
+if (num <= 2){
   document.querySelector("#video").src = "/videos/captain_america.mp4"
   document.querySelector("#audio").src = "/audios/captain_america.mp3"
 }
 
-else if (num > 4){
+else if (num <= 5){
   document.querySelector("#video").src = "/videos/ironman.mp4"
   document.querySelector("#audio").src = "/audios/ironman.mp3"
+}
+
+else if (num <= 7 ){
+  document.querySelector("#video").src = "/videos/hawkeye.mp4"
+  document.querySelector("#audio").src = "/audios/hawkeye.mp3"
+}
+
+else if (num <= 11){
+  document.querySelector("#video").src = "/videos/thor_3.mp4"
+  document.querySelector("#audio").src = "/audios/thor.mp3" 
 }
 
 
